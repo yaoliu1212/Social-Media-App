954b769360ce39f2422c9be66bf224a2b10e16ce
Yao Liu <liuyao201212@gmail.com>
2022-12-03 19:36:54 -0500

    basic get&post in server side

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import postRoutes from './routes/posts.js'
 //initialize app
 const app = express();
 
@@ -12,14 +12,14 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+// connect route to app
+app.use('/posts', postRoutes);
+
 //connect server application with database (mongodb)
 const CONNECTION_URL = 'mongodb+srv://mernSocialMedia:20001117lyPH@cluster0.vfci5zz.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
-
-// mongoose.connect(CONNECTION_URL).then(()=>{console.log('...')})
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-// mongoose.set('useFindAndModify', false);
