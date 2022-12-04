f00d781d8d0d40324b03ede144d9e2fdfcfc5d92
Yao Liu <liuyao201212@gmail.com>
2022-12-04 16:29:49 -0500

    redux setup with ReduxToolkits

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from './reducers/posts.js'
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore({
+    reducer: {
+        postsReducer,
+    }
+})
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,12 @@
+const postsReducers = (posts = [], action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return posts;
+        case 'CREATE':
+            return posts;
+        default:
+            return posts;
+    }
+}
+
+export default postsReducers;
